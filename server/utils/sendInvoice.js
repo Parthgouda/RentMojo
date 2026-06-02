141f30be6597c5c3b15d5673142492153ca51402
Parth gouda <vishnupriyagouda@gmail.com>
2026-06-02 12:24:25 +0530

    Fix SMTP configuration

--- server/utils/sendInvoice.js
+++ server/utils/sendInvoice.js
@@ -561,21 +561,14 @@ await new Promise(
 
 const transporter =
   nodemailer.createTransport({
-
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-
-      user:
-        process.env.EMAIL_USER,
-
-      pass:
-        process.env.EMAIL_PASS,
-
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
-
   });
-
 /* =========================
    SEND EMAIL
 ========================= */
